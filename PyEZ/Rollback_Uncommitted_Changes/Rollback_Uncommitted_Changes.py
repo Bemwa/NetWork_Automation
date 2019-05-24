94df588eb6c30726f665c9019ced986fd8742051
Bemwa Refaat Aziz <39416815+Bemwa@users.noreply.github.com>
2019-05-25 01:08:22 +0200

    Add files via upload

---
+++ PyEZ/Rollback_Uncommitted_Changes/Rollback_Uncommitted_Changes.py
@@ -0,0 +1,60 @@
+__author__ = "Bemwa Refaat Aziz"
+__EMAIL__ = "engbmwa@gmail.com"
+
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from jnpr.junos.exception import ConnectError
+import datetime
+import xlrd
+
+start_time = datetime.datetime.now().replace(microsecond=0)
+
+# get the devices info and credentials from sheet Excel
+# Excel sheet consist of three Col <ip address><username><password>
+
+workbook = xlrd.open_workbook("C:\\python_Scripts\\Rollback_Uncommitted_Changes\\hosts.xlsx")
+sheet = workbook.sheet_by_index(0)
+device_list = []
+for index in range(1, sheet.nrows):
+    ip_address = sheet.row(index)[0].value
+    user_name = sheet.row(index)[1].value
+    password = sheet.row(index)[2].value
+    device = {
+        'ip': ip_address,
+        'username': user_name,
+        'password': password,
+    }
+    device_list.append(device)
+q = 0
+# for loop to pass the device [] and use the credinitials to connect using PyEZ over port TCP 22
+for item in device_list:
+    dev = Device(host=item["ip"],user=item["username"],password=item["password"],port=22)
+    try:
+        dev.open()
+        print ("connected to " + dev.facts["hostname"])
+        q = q + 1
+        Box = Config(dev)
+        Uncommitted = Box.diff()
+        # if section to check if there are an Uncommitted Changes to be displayed before rollback
+        if Uncommitted:
+            print "There are an Uncommitted Changes"
+            print Uncommitted
+            Box.rollback()
+            print "Rollback Done"
+        else:
+            print "Uncommitted Changes Not Found, Exit"
+    except ConnectError as err:
+        print ("Cannot connect to device: {0}".format(err))
+    except Exception as err:
+        print (err)
+    try:
+        dev.close()
+    except Exception as err:
+        print (err)
+
+end_time = datetime.datetime.now().replace(microsecond=0)
+print ("total_time " + str(end_time - start_time))
+print ("no of junos devices " + str(q))
+z = raw_input(" Press Enter to Exit ")
+while z != "":
+    exit()
